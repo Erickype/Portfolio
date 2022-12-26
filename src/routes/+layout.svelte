b36c694a53da44213f3dc74dcc012e6585991db5
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-26 12:36:45 -0500

    Menu using buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,18 @@
 </script>
 
 <nav>
-	<a href="/">Home</a>
-	<a href="/email">Email</a>
-	<a href="/works">Works</a>
-	<a href="/resume">Resume</a>
+	<button class="bg-slate-300 hover:bg-slate-500 font-medium">
+		<a href="/">Home</a>
+	</button>
+	<button class="bg-slate-300 hover:bg-slate-500 font-medium">
+		<a href="/contact">Contact</a>
+	</button>
+	<button class="bg-slate-300 hover:bg-slate-500 font-medium">
+		<a href="/works">Works</a>
+	</button>
+	<button class="bg-slate-300 hover:bg-slate-500 font-medium">
+		<a href="/resume">Resume</a>
+	</button>
 </nav>
 
 <slot />
