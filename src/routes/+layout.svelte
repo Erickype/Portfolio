106c3cfbddda2d9434d2fa4f4aa181cdac1600cc
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-26 15:19:31 -0500

    Data map for use +layout.ts and +layout.server.ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,12 @@
 <script lang="ts">
 	import '../app.css';
-	import { onMount } from 'svelte';
-	import { pb } from '../lib/pocketbase';
+	import type { LayoutData } from './$types';
 
-	let menus: any = [];
-
-	onMount(async () => {
-		const menusList = pb.collection('menus').getList();
-
-		menus = (await menusList).items;
-	});
+	export let data: LayoutData;
 </script>
 
 <nav class="menus">
-	{#each menus as menu (menu.id)}
+	{#each data.posts as menu (menu.id)}
 		<button class="bg-slate-300 hover:bg-slate-500 font-medium">
 			<a href="/{menu.menuUrl}">{menu.menuName}</a>
 		</button>
