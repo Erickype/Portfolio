079125f9a6c8820e3025a25a4f4b45a60e7753d6
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-26 13:34:30 -0500

    Reactive menu using pocketbase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,23 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import { pb } from '../lib/pocketbase';
+
+	let menus: any = [];
+
+	onMount(async () => {
+		const menusList = pb.collection('menus').getList();
+
+		menus = (await menusList).items;
+	});
 </script>
 
-<nav>
-	<button class="bg-slate-300 hover:bg-slate-500 font-medium">
-		<a href="/">Home</a>
-	</button>
-	<button class="bg-slate-300 hover:bg-slate-500 font-medium">
-		<a href="/contact">Contact</a>
-	</button>
-	<button class="bg-slate-300 hover:bg-slate-500 font-medium">
-		<a href="/works">Works</a>
-	</button>
-	<button class="bg-slate-300 hover:bg-slate-500 font-medium">
-		<a href="/resume">Resume</a>
-	</button>
+<nav class="menus">
+	{#each menus as menu (menu.id)}
+		<button class="bg-slate-300 hover:bg-slate-500 font-medium">
+			<a href="/{menu.menuUrl}">{menu.menuName}</a>
+		</button>
+	{/each}
 </nav>
 
 <slot />
