3deca3fc6b2d2475bfaabc27d22ccf536bce3258
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-28 12:04:45 -0500

    Color change and test of the overflow in div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <div class="w-screen h-screen overflow-auto">
-	<div class="bg-darkBlue h-1/2 columns-2 py-10">
+	<div class="bg-blue h-1/2 columns-2 py-10">
+		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
+		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
+	</div>
+	<div class="bg-skyBlue h-1/2 columns-2 py-10">
 		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
 		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
 	</div>
 	<div class="bg-blue h-1/2 columns-2 py-10">
+		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
+		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
+	</div>
+	<div class="bg-skyBlue h-1/2 columns-2 py-10">
 		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
 		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
 	</div>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Resume</h1>
+<div class="w-screen h-screen overflow-auto">
+	<div class="bg-blue h-1/2 columns-2 py-10">
+		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
+		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
+	</div>
+	<div class="bg-skyBlue h-1/2 columns-2 py-10">
+		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
+		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
+	</div>
+	<div class="bg-blue h-1/2 columns-2 py-10">
+		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
+		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
+	</div>
+	<div class="bg-skyBlue h-1/2 columns-2 py-10">
+		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
+		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
+	</div>
+</div>
