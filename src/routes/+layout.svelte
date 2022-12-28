6670e92226ce5bbab358943491c82754aa9243f1
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-28 11:03:32 -0500

    Use of the new pallete and clean up

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,12 @@
 	export let data: LayoutData;
 </script>
 
-<div class="">
+<div class="bg-darkBlue">
 	<nav class="flex justify-center space-x-4">
 		{#each data.posts as menu (menu.id)}
 			<a
 				href="/{menu.menuUrl}"
-				class="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-200 hover:text-slate-900 active:bg-slate-300"
+				class="rounded-lg px-3 py-2 text-white font-medium hover:bg-green hover:text-blue active:bg-skyBlue"
 				>{menu.menuName}</a
 			>
 		{/each}
