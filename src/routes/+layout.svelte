2f7a2eb3fd29741a15e4bf6705a77616f52a66fe
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-28 12:01:40 -0500

    Footer added, and block overflow of the page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,16 +5,31 @@
 	export let data: LayoutData;
 </script>
 
-<div class="bg-darkBlue">
-	<nav class="flex justify-center space-x-4">
-		{#each data.posts as menu (menu.id)}
+<div class="h-screen max-h-screen static overflow-hidden">
+	<div class="bg-darkBlue columns-2">
+		<nav class="flex justify-start">
 			<a
-				href="/{menu.menuUrl}"
+				href="/"
 				class="rounded-lg px-3 py-2 text-white font-medium hover:bg-green hover:text-blue active:bg-skyBlue"
-				>{menu.menuName}</a
+				>Email</a
 			>
-		{/each}
-	</nav>
-</div>
+		</nav>
+		<nav class="flex justify-end space-x-4">
+			{#each data.posts as menu (menu.id)}
+				<a
+					href="/{menu.menuUrl}"
+					class="rounded-lg px-3 py-2 text-white font-medium hover:bg-green hover:text-blue active:bg-skyBlue"
+					>{menu.menuName}</a
+				>
+			{/each}
+		</nav>
+	</div>
 
-<slot />
+	<slot />
+
+	<div class="bg-darkBlue w-screen absolute bottom-0">
+		<footer class="text-white text-center font-semibold text-lg">
+			<a href="/">Footer</a>
+		</footer>
+	</div>
+</div>
