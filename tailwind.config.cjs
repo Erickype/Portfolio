37511f3cdf8a8553a42446cb57e88cfc0d3c9b0f
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-28 11:03:11 -0500

    Custom color pallete added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors:{
+      "darkBlue" : "#1f363d",
+      "blue" : "#40798c",
+      "skyBlue" : "#70a9a1",
+      "green" : "#9ec1a3",
+      "white" : "#cfe0c3",
+    },
     extend: {},
   },
   plugins: [],
