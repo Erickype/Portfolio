4d16ae826fc01f191ebb84149407670f68f855e8
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-29 11:35:03 -0500

    about, image, paragraph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,24 @@
 <div class="w-screen h-screen overflow-auto">
-	<div class="bg-blue h-1/2 columns-2 py-10">
-		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
-		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
-	</div>
-	<div class="bg-skyBlue h-1/2 columns-2 py-10">
-		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
-		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
-	</div>
-	<div class="bg-blue h-1/2 columns-2 py-10">
-		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
-		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
+	<div class="bg-blue h-1/2 columns-2 py-10 block flex-auto">
+		<div class="px-5 text-white overflow-hidden">
+			<h1 class=" text-center font-bold text-2xl flex-auto">Erick Carrasco</h1>
+			<p class="text-base py-2 flex-auto">
+				I am a skilled software engineer with experience in a variety of programming languages and
+				technologies. I am a team player who values collaboration and communication, and I enjoy
+				working with others to create innovative solutions. I am also highly self-motivated and able
+				to work independently, with a strong attention to detail and a commitment to delivering
+				high-quality software on time. Thank you for visiting my portfolio website. If you have any
+				questions or would like to discuss potential opportunities, please don't hesitate to get in
+				touch.
+			</p>
+		</div>
+		<div class="flex items-center justify-center h-auto min-h-full overflow-hidden">
+			<img
+				class="mx-auto"
+				src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6xMy3lYAo5Xb8tZ5rG4wWIDa-2zzfi8mE745Kr0YAMJkH3C9PvB7yZlZ96LXF8KE9MyE&usqp=CAU"
+				alt="portrait"
+			/>
+		</div>
 	</div>
 	<div class="bg-skyBlue h-1/2 columns-2 py-10">
 		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
