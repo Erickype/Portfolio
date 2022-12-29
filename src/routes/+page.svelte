51b7fce611092042f4bc839add7d2e4bd070550a
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-29 11:35:36 -0500

    clean up for sections part

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,5 @@
 		</div>
 	</div>
 	<div class="bg-skyBlue h-1/2 columns-2 py-10">
-		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
-		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
 	</div>
 </div>
