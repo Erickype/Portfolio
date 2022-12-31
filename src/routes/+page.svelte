7aff76a62f62099475502798f7db11ecad8240db
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-31 15:52:41 -0500

    Card to place content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col h-auto min-h-full overflow-hidden">
+<div class="flex flex-col h-auto min-h-full overflow-auto">
 	<div class="bg-blue h-2/5 py-10 inline-flex">
 		<div class="px-5 text-white overflow-hidden w-2/3">
 			<h1 class=" text-center font-bold text-2xl flex-auto">Erick Carrasco</h1>
@@ -20,49 +20,58 @@
 			/>
 		</div>
 	</div>
-	<div class="bg-blue h-2/5 py-10 inline-flex">
-		<div class="px-5 text-white overflow-hidden w-2/3">
-			<h1 class=" text-center font-bold text-2xl flex-auto">Erick Carrasco</h1>
-			<p class="text-base py-2 flex-auto">
-				I am a skilled software engineer with experience in a variety of programming languages and
-				technologies. I am a team player who values collaboration and communication, and I enjoy
-				working with others to create innovative solutions. I am also highly self-motivated and able
-				to work independently, with a strong attention to detail and a commitment to delivering
-				high-quality software on time. Thank you for visiting my portfolio website. If you have any
-				questions or would like to discuss potential opportunities, please don't hesitate to get in
-				touch.
-			</p>
+	<div class="bg-skyBlue grid grid-cols-4 text-darkBlue text-center text-lg flex-grow">
+		<div class="bg-white my-10 mx-5 rounded-xl p-4 relative">
+			<div class="text-xl font-bold mb-4">Card Title</div>
+			<div class="text-darkBlue mb-4 pb-10">
+				<p>
+					Card body content goes here Card body content goes here Card body content goes here Card
+					body content goes here Card body content goes here Card body content goes here Card body
+					content goes here
+				</p>
+			</div>
+			<div class="absolute left-0 right-0 bottom-0 p-4">
+				<button class="bg-blue text-white px-4 py-2 rounded-full basis-1/2">Action</button>
+			</div>
 		</div>
-		<div class="flex items-center justify-center h-auto min-h-full overflow-hidden w-1/3">
-			<img
-				class="mx-auto"
-				src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6xMy3lYAo5Xb8tZ5rG4wWIDa-2zzfi8mE745Kr0YAMJkH3C9PvB7yZlZ96LXF8KE9MyE&usqp=CAU"
-				alt="portrait"
-			/>
+		<div class="bg-green my-10 mx-5 rounded-xl p-4 relative">
+			<div class="text-xl font-bold mb-4">Card Title</div>
+			<div class="text-darkBlue mb-4 pb-10">
+				<p>
+					Card body content goes here Card body content goes here Card body content goes here Card
+					body content goes here Card body content goes here Card body content goes here Card body
+					content goes here
+				</p>
+			</div>
+			<div class="absolute left-0 right-0 bottom-0 p-4">
+				<button class="bg-blue text-white px-4 py-2 rounded-full basis-1/2">Action</button>
+			</div>
 		</div>
-	</div>
-	<div class="bg-skyBlue grid grid-cols-4 text-darkBlue text-center text-lg flex-grow">
-		<div class="bg-white my-10">1</div>
-		<div class="mx-auto bg-green w-1/4">2</div>
-		<div class="mx-auto bg-white w-auto min-w-full">3</div>
-		<div class="mx-auto bg-green w-auto min-w-full">4</div>
-	</div>
-	<div class="bg-skyBlue grid grid-cols-4 text-darkBlue text-center text-lg flex-grow">
-		<div class="bg-white my-10">1</div>
-		<div class="mx-auto bg-green w-1/4">2</div>
-		<div class="mx-auto bg-white w-auto min-w-full">3</div>
-		<div class="mx-auto bg-green w-auto min-w-full">4</div>
-	</div>
-	<div class="bg-skyBlue grid grid-cols-4 text-darkBlue text-center text-lg flex-grow">
-		<div class="bg-white my-10">1</div>
-		<div class="mx-auto bg-green w-1/4">2</div>
-		<div class="mx-auto bg-white w-auto min-w-full">3</div>
-		<div class="mx-auto bg-green w-auto min-w-full">4</div>
-	</div>
-	<div class="bg-skyBlue grid grid-cols-4 text-darkBlue text-center text-lg flex-grow">
-		<div class="bg-white my-10">1</div>
-		<div class="mx-auto bg-green w-1/4">2</div>
-		<div class="mx-auto bg-white w-auto min-w-full">3</div>
-		<div class="mx-auto bg-green w-auto min-w-full">4</div>
+		<div class="bg-white my-10 mx-5 rounded-xl p-4 relative">
+			<div class="text-xl font-bold mb-4">Card Title</div>
+			<div class="text-darkBlue mb-4 pb-10">
+				<p>
+					Card body content goes here Card body content goes here Card body content goes here Card
+					body content goes here Card body content goes here Card body content goes here Card body
+					content goes here
+				</p>
+			</div>
+			<div class="absolute left-0 right-0 bottom-0 p-4">
+				<button class="bg-blue text-white px-4 py-2 rounded-full basis-1/2">Action</button>
+			</div>
+		</div>
+		<div class="bg-green my-10 mx-5 rounded-xl p-4 relative">
+			<div class="text-xl font-bold mb-4">Card Title</div>
+			<div class="text-darkBlue mb-4 pb-10">
+				<p>
+					Card body content goes here Card body content goes here Card body content goes here Card
+					body content goes here Card body content goes here Card body content goes here Card body
+					content goes here
+				</p>
+			</div>
+			<div class="absolute left-0 right-0 bottom-0 p-4">
+				<button class="bg-blue text-white px-4 py-2 rounded-full basis-1/2">Action</button>
+			</div>
+		</div>
 	</div>
 </div>
