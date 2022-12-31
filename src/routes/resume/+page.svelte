42e9519d886ab2bcd9b60fd020b08e85229bdfaf
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-31 12:54:22 -0500

    test of the overflow content div in other pages

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,5 +1,5 @@
-<div class="w-screen h-screen overflow-auto">
-	<div class="bg-blue h-1/2 columns-2 py-10">
+<div class="h-auto min-h-full overflow-hidden">
+	<div class="bg-blue h-screen columns-2 py-10">
 		<h1 class="text-white text-center font-bold text-4xl">Erick Carrasco</h1>
 		<h1 class="text-white text-center font-bold text-xl">Erick Carrasco</h1>
 	</div>
