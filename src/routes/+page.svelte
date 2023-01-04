cd230fb4c4a24f26f9da25db3cb6cc9f9c260ccf
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-04 16:24:39 -0500

    block of the overflow in x

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,16 @@
 	export let data: PageServerData;
 </script>
 
-<div class="flex flex-col h-auto min-h-full overflow-auto">
-	<div class="bg-blue h-2/5 py-10 inline-flex">
-		<div class="px-5 text-white overflow-hidden w-2/3">
-			<h1 class=" text-center font-bold text-2xl flex-auto">Erick Carrasco</h1>
-			<p class="text-base py-2 flex-auto">
-				I am a skilled software engineer with experience in a variety of programming languages and
-				technologies. I am a team player who values collaboration and communication, and I enjoy
-				working with others to create innovative solutions. I am also highly self-motivated and able
-				to work independently, with a strong attention to detail and a commitment to delivering
-				high-quality software on time. Thank you for visiting my portfolio website. If you have any
-				questions or would like to discuss potential opportunities, please don't hesitate to get in
-				touch.
-			</p>
+<div class="flex flex-col h-auto min-h-full overflow-x-hidden overflow-y-auto">
+	<div class="bg-blue h-2/5 py-10 inline-flex px-5">
+		<div class="px-5 text-white overflow-hidden w-2/3 py-5">
+			<h1 class=" text-left flex-auto">
+				<p class="text-lg  sm:text- md:text-xl lg:text-4xl font-bold">
+					Hello, <br />
+					I'm Erick <br />
+					Software Engineer
+				</p>
+			</h1>
 		</div>
 		<div class="flex items-center justify-center h-auto min-h-full overflow-hidden w-1/3">
 			<img
@@ -26,9 +23,7 @@
 			/>
 		</div>
 	</div>
-	<div
-		class="bg-skyBlue grid grid-cols-3 text-darkBlue text-center text-lg flex-grow"
-	>
+	<div class="bg-skyBlue grid grid-cols-2 text-darkBlue text-center text-lg flex-grow">
 		{#each data.contents as content (content.id)}
 			<div class="bg-white my-10 mx-5 rounded-xl p-4 relative">
 				<div class="text-xl font-bold mb-4">{content.titleContent}</div>
