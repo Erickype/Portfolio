fc3b227c9e5bda03b70254a5557214b55e5c00a4
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-08 18:33:51 -0500

    fiel for functions fetchMenuContent

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,44 +1,11 @@
-import type { IMenu, IMenuContents } from "$lib/interfaces/iMenu";
-import { pb } from "$lib/pocketbase";
 import type { PageServerLoad } from "./$types";
-import { page } from "$app/stores";
+import { homeName } from "$lib/constants";
+import { fetchMenuContent } from "$lib/functions";
 
-export const load = (async () => {
+export const load = (async ({ parent }) => {
+    const menus = await parent()
+    const menu = menus.menus.find(menu => menu.menuName === homeName)!.id
     return {
-        contents: await fetchMenuContent()
+        contents: await fetchMenuContent(menu)
     }
 }) satisfies PageServerLoad
-
-export function getMenuId(menuName: string): string {
-
-    let menuId: string = ""
-
-    page.subscribe((value) => {
-        let menus = (value.data.menus) as IMenu[];
-        menuId = menus.find(menu => menu.menuName === menuName)!.id
-    });
-
-    return menuId
-}
-
-async function fetchMenuContent() {
-
-    const menuId = getMenuId("Resume")
-    console.log(menuId);
-    
-
-    const response = await pb.collection("mainContents").getList<IMenuContents>(1, 10, {
-        filter: 'menuId = "'+menuId+'"',
-    });
-
-    const data = response.items.map((res) => {
-        return {
-            id: res.id,
-            titleContent: res.titleContent,
-            bodyContent: res.bodyContent,
-            actionContent: res.actionContent
-        }
-    })
-
-    return data
-}
