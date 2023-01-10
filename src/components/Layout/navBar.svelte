51d1672cc6dbf35edbb0fae3b05b3a100ba584c2
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-10 12:18:21 -0500

    navbar layout component

---
+++ src/components/Layout/navBar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let data: { menus: any; }
+</script>
+
+<div class="bg-darkBlue columns-2">
+    <nav class="flex justify-start">
+        <a
+            href="/"
+            class="rounded-lg px-3 py-2 text-white font-medium hover:bg-green hover:text-blue active:bg-skyBlue"
+            >Email</a
+        >
+    </nav>
+    <nav class="flex justify-end space-x-4">
+        {#each data.menus as menu (menu.id)}
+            <a
+                href="/{menu.menuUrl}"
+                class="rounded-lg px-3 py-2 text-white font-medium hover:bg-green hover:text-blue active:bg-skyBlue"
+                >{menu.menuName}</a
+            >
+        {/each}
+    </nav>
+</div>
