aad7d0dee045ef58bba9f86901bf7110598fc9fb
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-14 16:36:30 -0500

    Data fetch of moreAboutContent

--- src/routes/more/[slug]/+page.server.ts
+++ src/routes/more/[slug]/+page.server.ts
@@ -1,4 +1,5 @@
 import type { IMoreAboutContent } from '$lib/interfaces/iMenu';
+import { pb } from '$lib/pocketbase';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
@@ -9,11 +10,26 @@ export const load = (async ({ params }) => {
             moreAboutContent: content
         };
     }
-
     throw error(404, 'Not found');
 }) satisfies PageServerLoad
 
 async function getMoreAboutContent(slug: string) {
-    const content: IMoreAboutContent[] = [{ id: "Test", description: "Test", projectName: "test", url: "test", mainContentId: "1" }]
-    return content
+    const record = await pb.collection('mainContents').getFirstListItem('titleContent="' + slug + '"');
+    const resultList = await pb.collection('moreAboutContent').getList<IMoreAboutContent>(1, 50, {
+        filter: 'mainContentId = "' + record.id + '"'
+    });
+
+    const data = resultList.items.map((res) => {
+        const item: IMoreAboutContent = {
+            id: res.id,
+            projectName: res.projectName,
+            description: res.description,
+            url: res.url,
+            image: res.image,
+            mainContentId: res.mainContentId
+        }
+        return item
+    })
+
+    return data
 }
