69e5761d266d9b7521f0e65abdd0dc6e9b200430
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-14 14:08:01 -0500

    More slug file to more about technologies

---
+++ src/routes/more/[slug]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>{data.menus[0].menuName}</h1>
+<div>{@html data.menus[0].menuName}</div>
