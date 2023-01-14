dfd66157bee6495d1e889aad5ebfbdb7e50d15ff
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-14 14:50:52 -0500

    Use of the dataload

--- src/routes/more/[slug]/+page.svelte
+++ src/routes/more/[slug]/+page.svelte
@@ -2,7 +2,9 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+    console.log(data.moreAboutContent[0]);
+    
 </script>
 
-<h1>{data.menus[0].menuName}</h1>
-<div>{@html data.menus[0].menuName}</div>
+<h1>{data.moreAboutContent[0].projectName}</h1>
+<div>{@html data.moreAboutContent[0].description}</div>
