61285b998c5fda8422c1cfe4bcf5087465e2dd44
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-15 02:29:26 -0500

    Mailto and center of text

--- src/components/Layout/footer.svelte
+++ src/components/Layout/footer.svelte
@@ -1,9 +1,15 @@
-<footer>
-	<strong>Erick Carrasco</strong>
-</footer>
+<main class="container">
+	<footer>
+		<hgroup>
+			<h5>Erick Carrasco</h5>
+			<h6><a href="mailto:erickype@hotmail.com">erickype@hotmail.com</a></h6>
+		</hgroup>
+	</footer>
+</main>
 
 <style>
 	footer {
 		margin-bottom: 1em;
+		text-align: center;
 	}
 </style>
