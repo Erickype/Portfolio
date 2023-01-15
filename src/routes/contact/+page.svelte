2bb7f400cbfb5d513452e4fb87143393ba7fd1f5
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-15 18:09:20 -0500

    Add of article and remove lastname

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,27 +1,28 @@
 <h1>Contact</h1>
-<form>
-	<!-- Grid -->
-	<div class="grid">
-		<!-- Markup example 1: input is inside label -->
-		<label for="firstname">
-			First name
-			<input type="text" id="firstname" name="firstname" placeholder="First name" required />
-		</label>
 
-		<label for="lastname">
-			Last name
-			<input type="text" id="lastname" name="lastname" placeholder="Last name" required />
+<article>
+	<form>
+		<label for="name">
+			Your name
+			<input type="text" id="name" name="firstname" placeholder="First name" required />
 		</label>
-	</div>
-	<label for="comment">
-		Your message
-        <textarea id="comment" name="comment" placeholder="Your message" required rows="2" cols="40"></textarea>
-	</label>
-	<!-- Markup example 2: input is after label -->
-	<label for="email">Email address</label>
-	<input type="email" id="email" name="email" placeholder="Email address" required />
-	<small>I'll never share your email with anyone else.</small>
+		<label for="comment">
+			Your message
+			<textarea
+				id="comment"
+				name="comment"
+				placeholder="Your message"
+				required
+				rows="2"
+				cols="40"
+			/>
+		</label>
+		<!-- Markup example 2: input is after label -->
+		<label for="email">Email address</label>
+		<input type="email" id="email" name="email" placeholder="Email address" required />
+		<small>I'll never share your email with anyone else.</small>
 
-	<!-- Button -->
-	<button type="submit">Submit</button>
-</form>
+		<!-- Button -->
+		<button type="submit">Submit</button>
+	</form>
+</article>
