e73f6d2606d019f35631338d6b48589472032bf9
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-21 18:30:28 -0500

    ProjectContent component

---
+++ src/components/projectContentCard.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { imageStoreUrl } from "$lib/constants";
+	import type { IMoreAboutContent } from "$lib/interfaces/iMenu";
+
+    export let data: IMoreAboutContent[]
+</script>
+
+{#each data as moreContent, i}
+	<article>
+		<div class="grid">
+			{#if i % 2 == 0}
+				<div>
+					<h4>{moreContent.projectName}</h4>
+					<p>{moreContent.description}</p>
+					<a href={moreContent.url}>Github</a>
+				</div>
+				<div class="imageCell">
+					<img
+						src="{imageStoreUrl}/{moreContent.id}/{moreContent.image}"
+						alt={moreContent.projectName}
+					/>
+				</div>
+			{:else}
+				<div class="imageCell">
+					<img
+						src="{imageStoreUrl}/{moreContent.id}/{moreContent.image}"
+						alt={moreContent.projectName}
+					/>
+				</div>
+				<div>
+					<h4>{moreContent.projectName}</h4>
+					<p>{moreContent.description}</p>
+					<a href={moreContent.url}>Github</a>
+				</div>
+			{/if}
+		</div>
+	</article>
+{:else}
+	<h1>There is nothing here, ups!</h1>
+{/each}
+
+<style>
+	.imageCell {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	img {
+		margin: auto;
+	}
+	article {
+		padding: 1em;
+		margin: 1em;
+	}
+</style>
