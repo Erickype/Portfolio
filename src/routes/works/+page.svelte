a701d508b2882c21f2c1bd7e577b252ff98492b6
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-21 18:39:29 -0500

    Use of the Projects content component

---
+++ src/routes/works/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+    import ProjectsContent from "../../components/projectContentCard.svelte"
+
+    export let data : PageData
+</script>
+
+<h1>My projects</h1>
+
+<ProjectsContent data = {data.moreAboutContent}></ProjectsContent>
