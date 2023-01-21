e2829d6ebd93502a243c497ef98f7234701ec5af
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-21 18:30:37 -0500

    Use of the project component

--- src/routes/more/[slug]/+page.svelte
+++ src/routes/more/[slug]/+page.svelte
@@ -1,64 +1,11 @@
 <script lang="ts">
-	import { imageStoreUrl } from '$lib/constants';
 	import type { PageData } from './$types';
 	import { page } from '$app/stores';
+	import ProjectsContent from "../../../components/projectContentCard.svelte";
 
 	export let data: PageData;
-	
 </script>
 
 <h3>{$page.params.slug} Projects</h3>
 
-{#each data.moreAboutContent as moreContent, i}
-	<main class="container">
-		<article>
-			<div class="grid">
-				{#if i % 2 == 0}
-					<div>
-						<h4>{moreContent.projectName}</h4>
-						<p>{moreContent.description}</p>
-						<a href={moreContent.url}>Github</a>
-					</div>
-					<div class="imageCell">
-						<img
-							src="{imageStoreUrl}/{moreContent.id}/{moreContent.image}"
-							alt={moreContent.projectName}
-						/>
-					</div>
-				{:else}
-					<div class="imageCell">
-						<img
-							src="{imageStoreUrl}/{moreContent.id}/{moreContent.image}"
-							alt={moreContent.projectName}
-						/>
-					</div>
-					<div>
-						<h4>{moreContent.projectName}</h4>
-						<p>{moreContent.description}</p>
-						<a href={moreContent.url}>Github</a>
-					</div>
-				{/if}
-			</div>
-		</article>
-	</main>
-{:else}
-	<h1>There is nothing here, ups!</h1>
-{/each}
-
-<style>
-	.container {
-		margin-top: 20px;
-	}
-	.imageCell {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-	img {
-		margin: auto;
-	}
-	article {
-		padding: 1em;
-		margin: 1em;
-	}
-</style>
+<ProjectsContent data = {data.moreAboutContent}></ProjectsContent>
