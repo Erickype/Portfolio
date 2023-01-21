697ff3dea8cef79db18925057a898d9a6d088c37
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-20 21:51:26 -0500

    Import of the app.css file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import '@picocss/pico';
+	import '../app.css'
+
 	import type { LayoutData } from './$types';
 	import NavBar from '../components/Layout/navBar.svelte';
 	import Footer from '../components/Layout/footer.svelte';
