2172ebd58a78cb6cdcaf8bf4c2006679c889d653
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-21 19:21:19 -0500

    Style of the content in a card

--- src/components/projectContentCard.svelte
+++ src/components/projectContentCard.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-	import { imageStoreUrl } from "$lib/constants";
-	import type { IMoreAboutContent } from "$lib/interfaces/iMenu";
+	import { imageStoreUrl } from '$lib/constants';
+	import type { IMoreAboutContent } from '$lib/interfaces/iMenu';
 
-    export let data: IMoreAboutContent[]
+	export let data: IMoreAboutContent[];
 </script>
 
 {#each data as moreContent, i}
 	<article>
 		<div class="grid">
 			{#if i % 2 == 0}
-				<div>
-					<h4>{moreContent.projectName}</h4>
-					<p>{moreContent.description}</p>
-					<a href={moreContent.url}>Github</a>
-				</div>
+				<article class="contentArticle">
+					<header>
+						<h5>{moreContent.projectName}</h5>
+					</header>
+
+					<div id="content">
+						<p>{moreContent.description}</p>
+						<a href={moreContent.url}>Github</a>
+					</div>
+				</article>
 				<div class="imageCell">
 					<img
 						src="{imageStoreUrl}/{moreContent.id}/{moreContent.image}"
@@ -27,11 +32,16 @@
 						alt={moreContent.projectName}
 					/>
 				</div>
-				<div>
-					<h4>{moreContent.projectName}</h4>
-					<p>{moreContent.description}</p>
-					<a href={moreContent.url}>Github</a>
-				</div>
+				<article class="contentArticle">
+					<header>
+						<h5>{moreContent.projectName}</h5>
+					</header>
+
+					<div id="content">
+						<p>{moreContent.description}</p>
+						<a href={moreContent.url}>Github</a>
+					</div>
+				</article>
 			{/if}
 		</div>
 	</article>
@@ -51,5 +61,31 @@
 	article {
 		padding: 1em;
 		margin: 1em;
+	}
+	.contentArticle {
+		margin: 0;
+		padding: 0;
+	}
+	header {
+		margin: 0;
+		padding: 0;
+		text-align: center;
+	}
+	h5 {
+		margin: auto;
+		padding: auto;
+		padding-top: 0.5em;
+		padding-bottom: 0.5em;
+	}
+	#content {
+		height: 85%;
+		padding: 1em;
+		position: relative;
+	}
+	a {
+		position: absolute;
+		bottom: 1em;
+		text-align: center;
+		margin: auto;
 	}
 </style>
