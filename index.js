2cd355c3d7f1f9bce773d9b9cb8bf87f84ea1315
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-25 13:46:04 -0500

    Updates

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const error_template = ({ status, message }) => "<!DOCTYPE html>\n<html lang=\"e
 let read = null;
 
 set_paths({"base":"","assets":""});
-set_version("1674671118128");
+set_version("1674672339735");
 
 let default_protocol = 'https';
 
@@ -50,7 +50,7 @@ export class Server {
 			app_template,
 			app_template_contains_nonce: false,
 			error_template,
-			version: "1674671118128"
+			version: "1674672339735"
 		};
 	}
 
