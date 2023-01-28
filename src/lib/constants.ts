bb1fa6d5395cafba607239bad728eae55c6b31b2
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-28 17:47:24 -0500

    Image url constant remote

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 //Image Store url
-export const imageStoreUrl = "http://127.0.0.1:8090/api/files/9yaizekszlbdanb"
+export const imageStoreUrl = "https://ecpocketbase.fly.dev/api/files/9yaizekszlbdanb"
 
 export const homeName = "Home"
