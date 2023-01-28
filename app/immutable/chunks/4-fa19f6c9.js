edfc0a9b50af78d668c2ee9fb7a345a0c9ab41f8
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-28 17:46:21 -0500

    Updates

--- app/immutable/chunks/4-fa19f6c9.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/more/_slug_/_page.svelte-3a2e9588.js";const e=!0;export{t as component,e as server};
---
+++ app/immutable/chunks/6-b3d2b0e3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/works/_page.svelte-bcff48c9.js";const e=!0;export{t as component,e as server};
