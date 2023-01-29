a526477d24600b0521f0319ddb97f900c47b6600
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-29 16:48:47 -0500

    Updates

---
+++ app/immutable/chunks/5-2bc11f64.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/resume/_page.svelte-d0724703.js";export{t as component};
--- app/immutable/chunks/5-8e31c686.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/resume/_page.svelte-8283059f.js";export{t as component};
