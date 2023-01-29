e53b54473fd6ec2ceb40a2939658e491ad347cfb
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-29 14:13:27 -0500

    Updates

--- app/immutable/chunks/5-4aa2cd16.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/resume/_page.svelte-a2f65913.js";export{t as component};
--- app/immutable/components/pages/resume/_page.svelte-a2f65913.js
+++
@@ -1,1 +0,0 @@
-import{S as e,i as t,s as a}from"../../../chunks/index-d11e8e7c.js";class o extends e{constructor(s){super(),t(this,s,null,null,a,{})}}export{o as default};
