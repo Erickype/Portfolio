99840498fa29aa3f08dadf10acbacc6b348223f7
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-29 14:12:13 -0500

    Basic style and education section

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,29 @@
+<hgroup class="title">
+    <h1>Erick Josué Carrasco Santos</h1>
+    <h3>+593 996540761</h3>
+</hgroup>
+
+<article>
+	<h2>Education</h2>
+	<div class="grid">
+		<hgroup>
+			<h4>Universidad Técnica de Ambato</h4>
+			<h5>Software Engineer</h5>
+		</hgroup>
+	</div>
+</article>
+
+<style>
+    h2{
+        margin-bottom: 5px;
+    }
+    .title{
+        text-align: center;
+        margin-bottom: 5px;
+    }
+    article{
+        margin-top: 20px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+</style>
