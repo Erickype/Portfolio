fcf1de4dbb332bd819b2da87f70d6edb81765aff
Erickype <67160282+Erickype@users.noreply.github.com>
2023-01-29 14:30:19 -0500

    svg phone to the number

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,6 +1,22 @@
 <hgroup class="title">
-    <h1>Erick Josué Carrasco Santos</h1>
-    <h3>+593 996540761</h3>
+	<h1>Erick Josué Carrasco Santos</h1>
+	<h3>
+		<svg class="icon" viewBox="0 0 20 20">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				id="Layer_1"
+				x="0"
+				y="0"
+				version="1.1"
+				viewBox="0 0 29 29"
+				xml:space="preserve"
+				><path
+					d="m20.914 17.743-2.091 1.178a1.319 1.319 0 0 1-1.58-.217l-6.979-6.979a1.32 1.32 0 0 1-.217-1.58l1.178-2.091a1.978 1.978 0 0 0-.325-2.37L7.766 2.55a1.978 1.978 0 0 0-2.798 0L3.545 3.972a5.276 5.276 0 0 0-.793 6.446l.714 1.19a41.36 41.36 0 0 0 14.946 14.631l.141.081c2.102 1.201 4.699.851 6.382-.831l1.486-1.486a1.978 1.978 0 0 0 0-2.798l-3.136-3.136a1.978 1.978 0 0 0-2.371-.326z"
+				/></svg
+			>
+		</svg>
+		+593 996540761
+	</h3>
 </hgroup>
 
 <article>
@@ -14,16 +30,20 @@
 </article>
 
 <style>
-    h2{
-        margin-bottom: 5px;
-    }
-    .title{
-        text-align: center;
-        margin-bottom: 5px;
-    }
-    article{
-        margin-top: 20px;
-        padding-top: 20px;
-        padding-bottom: 20px;
-    }
+	h2 {
+		margin-bottom: 5px;
+	}
+	.title {
+		text-align: center;
+		margin-bottom: 5px;
+	}
+	article {
+		margin-top: 20px;
+		padding-top: 20px;
+		padding-bottom: 20px;
+	}
+	.icon {
+		height: 1em;
+		margin-right: 0.5em;
+	}
 </style>
