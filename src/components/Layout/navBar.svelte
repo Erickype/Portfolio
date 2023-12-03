d90a9afbb15cb5b608558bdf6131e06834959986
Erick Carrasco <erickype@hotmail.com>
2023-12-03 23:15:13 +0000

    Navbar updated

--- src/components/Layout/navBar.svelte
+++ src/components/Layout/navBar.svelte
@@ -6,14 +6,20 @@
 </script>
 
 <nav class="navbar bg-base-200">
-	<ul />
-	<ul>
-		{#each data.menus as menu (menu.id)}
-			<li>
-				<a href="{base}/{menu.menuUrl}" role="button">
-					{menu.menuName}
-				</a>
-			</li>
-		{/each}
-	</ul>
+	<div class="flex-1">
+		<a class="btn btn-ghost text-xl" href="{base}/"> Erick's </a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-4">
+			{#each data.menus as menu (menu.id)}
+				{#if menu.menuUrl != ''}
+					<li>
+						<a href="{base}/{menu.menuUrl}" role="button">
+							{menu.menuName}
+						</a>
+					</li>
+				{/if}
+			{/each}
+		</ul>
+	</div>
 </nav>
