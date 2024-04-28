04ba6c5ce4c19d3959c4e8fcc2262be900ca411a
Erick Carrasco <erickype@hotmail.com>
2024-04-28 20:13:45 +0000

    Hero responsive

--- src/lib/components/Home/hero.svelte
+++ src/lib/components/Home/hero.svelte
@@ -8,10 +8,12 @@
     import * as Avatar from "$lib/components/ui/avatar/index.js";
 </script>
 
-<div class="flex flex-1 justify-center items-center gap-6 min-h-fit h-[80vh]">
-    <div class="w-1/2">
+<div
+    class="flex flex-col lg:flex-row justify-center items-center gap-6 lg:min-h-fit lg:h-[80vh]"
+>
+    <div class="px-4 lg:w-1/2">
         <h1
-            class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
+            class="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl"
         >
             <div class="flex items-center gap-2 mb-2">
                 <p>
@@ -20,13 +22,13 @@
                     <span class="text-primary">Erick Carrasco</span>
                 </p>
                 <img
-                    class="w-14 h-14"
+                    class="w-10 h-10 lg:w-14 lg:h-14"
                     src={CodeBrakets}
                     alt="Code brakets gif"
                 />
             </div>
             <code
-                class="relative rounded bg-muted px-2 py-2 font-mono text-xl font-semibold"
+                class="relative rounded bg-muted px-2 py-2 font-mono text-sm lg:text-xl font-semibold"
             >
                 Software Engineer
             </code>
@@ -37,8 +39,8 @@
             project.
         </p>
     </div>
-    <div class="flex flex-col w-1/2 justify-center items-center">
-        <Avatar.Root class="w-60 h-60">
+    <div class="flex flex-col lg:w-1/2 justify-center items-center">
+        <Avatar.Root class="w-24 h-24 lg:w-60 lg:h-60">
             <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
             <Avatar.Fallback>EC</Avatar.Fallback>
         </Avatar.Root>
