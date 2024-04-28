e2e06b215b1021048196153ac6fc69da8f38977b
Erick Carrasco <erickype@hotmail.com>
2024-04-28 19:51:57 +0000

    Hero component with social links

---
+++ src/lib/components/Home/hero.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    import CodeBrakets from "$lib/assets/codeBrakets.gif";
+    import Mail from "lucide-svelte/icons/mail";
+    import LinkedIn from "lucide-svelte/icons/linkedin";
+    import Github from "lucide-svelte/icons/github";
+
+    import { Button } from "$lib/components/ui/button/index.js";
+    import * as Avatar from "$lib/components/ui/avatar/index.js";
+</script>
+
+<div class="flex flex-1 justify-center items-center gap-6 min-h-fit h-[80vh]">
+    <div class="w-1/2">
+        <h1
+            class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
+        >
+            <div class="flex items-center gap-2 mb-2">
+                <p>
+                    Hi!, I'm
+                    <br />
+                    <span class="text-primary">Erick Carrasco</span>
+                </p>
+                <img
+                    class="w-14 h-14"
+                    src={CodeBrakets}
+                    alt="Code brakets gif"
+                />
+            </div>
+            <code
+                class="relative rounded bg-muted px-2 py-2 font-mono text-xl font-semibold"
+            >
+                Software Engineer
+            </code>
+        </h1>
+        <p class="py-6 leading-7 [&:not(:first-child)]:mt-6">
+            Experienced in different areas of software development. Excited to
+            join a team of passionate individuals and work in an engaging
+            project.
+        </p>
+    </div>
+    <div class="flex flex-col w-1/2 justify-center items-center">
+        <Avatar.Root class="w-60 h-60">
+            <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+            <Avatar.Fallback>EC</Avatar.Fallback>
+        </Avatar.Root>
+        <div class="flex mt-6 px-6 py-4 gap-4">
+            <Button
+                href="https://www.linkedin.com/in/erick-carrasco-santos"
+                variant="outline"
+                size="icon"
+            >
+                <LinkedIn class="h-4 w-4" />
+            </Button>
+            <Button
+                href="https://github.com/Erickype"
+                variant="outline"
+                size="icon"
+            >
+                <Github class="h-4 w-4" />
+            </Button>
+            <Button
+                href="mailto:erickype@hotmail.com"
+                variant="outline"
+                size="icon"
+            >
+                <Mail class="h-4 w-4" />
+            </Button>
+        </div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,8 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import CodeBrakets from "$lib/assets/codeBrakets.gif";
-
-	let projectsSection: HTMLElement | null;
-
-	function scrollToLowerSection() {
-		projectsSection!.scrollIntoView({ behavior: "smooth" });
-	}
-
-	onMount(() => {
-		projectsSection = document.getElementById("projectsSection");
-	});
+	import Hero from "$lib/components/Home/hero.svelte";
 </script>
 
-<!-- <div class="hero min-h-screen pr-16 pl-16">
-	<div class="hero-content flex-col lg:flex-row-reverse">
-		<div>
-			<h1 class="text-5xl text-info font-bold">Hi, I'm Erick software engineer</h1>
-			<p class="py-6">
-				I'm a young software engineer an tech enthusiast with experience in different areas of
-				software development. I am also excited to join a team of passionate individuals for an
-				engaging project.
-			</p>
-			<button class="btn btn-primary" on:click={scrollToLowerSection}>About me</button>
-		</div>
-	</div>
-</div> -->
-
-<div class="flex justify-center items-center gap-6 min-h-fit h-[80vh]">
-	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-		<div class="flex items-center gap-2 mb-2">
-			<p>
-				Hi!, I'm
-				<br />
-				<span class="text-primary">Erick Carrasco</span>
-			</p>
-			<img class="w-14 h-14" src={CodeBrakets} alt="Code brakets gif" />
-		</div>
-		<code
-			class="relative rounded bg-muted px-2 py-2 font-mono text-xl font-semibold"
-		>
-			Software Engineer
-		</code>
-	</h1>
-</div>
+<Hero></Hero>
 
 <!-- <div id="projectsSection" class="flex flex-col items-center min-h-screen">
 	<h4 class="text-2xl font-bold text-success">Principal skills</h4>
