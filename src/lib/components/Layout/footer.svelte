76749fb3859d94251cc73a012700314a51ba9612
Erick Carrasco <erickype@hotmail.com>
2024-04-29 16:09:46 +0000

    Basic footer

--- src/lib/components/Layout/footer.svelte
+++ src/lib/components/Layout/footer.svelte
@@ -1,12 +1,29 @@
-<footer class="footer p-10 bg-base-200 text-base-content">
-	<aside>
-		<h4 class="text-2xl font-bold">Erick Carrasco</h4>
-		<a href="mailto:erickype@hotmail.com">erickype@hotmail.com</a>
-	</aside>
-	<nav>
-		<header class="footer-title">Content</header>
-		<a class="link link-hover" href="/resume">About me</a>
-		<a class="link link-hover" href="/resume">Experience</a>
-		<a class="link link-hover" href="/works">Works</a>
-	</nav>
+<script lang="ts">
+</script>
+
+<footer class="bg-background px-6 py-10 lg:px-40 lg:py-14">
+	<div class="flex flex-col">
+		<h4 class="text-2xl font-semibold">Erick Carrasco</h4>
+		<a
+			class="text-muted-foreground transition-colors hover:text-foreground"
+			href="mailto:erickype@hotmail.com">erickype@hotmail.com</a
+		>
+	</div>
+	<div class="flex flex-col mt-4">
+		<header class="text-2xl font-semibold">Content</header>
+		<div class="flex flex-col gap-2 justify-between">
+			<a
+				class="text-muted-foreground transition-colors hover:text-foreground"
+				href="/">Home</a
+			>
+			<a
+				class="text-muted-foreground transition-colors hover:text-foreground"
+				href="/projects">Projects</a
+			>
+			<a
+				class="text-muted-foreground transition-colors hover:text-foreground"
+				href="/about">About me</a
+			>
+		</div>
+	</div>
 </footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import { ModeWatcher } from "mode-watcher";
 
 	import NavBar from "$lib/components/Layout/navBar.svelte";
+	import Footer from "$lib/components/Layout/footer.svelte";
 
 	let title = "Erick Carrasco";
 </script>
@@ -14,10 +15,12 @@
 
 <ModeWatcher />
 
-<div class="flex min-h-screen w-full flex-col">
+<div class="flex min-h-screen w-full flex-col bg-muted/40">
 	<NavBar></NavBar>
 
 	<main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 lg:mx-32 h-full">
 		<slot></slot>
 	</main>
+
+	<Footer></Footer>
 </div>
