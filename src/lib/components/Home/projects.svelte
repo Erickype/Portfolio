94ae26e198f40efa44cb2c1f7aaa8c1cd1d30c86
Erick Carrasco <erickype@hotmail.com>
2024-05-01 01:20:21 +0000

    Basic personal projects section

--- src/lib/components/Home/projects.svelte
+++ src/lib/components/Home/projects.svelte
@@ -1,32 +1,37 @@
 <script lang="ts">
     import * as Card from "$lib/components/ui/card/index.js";
-    import type { Experience } from "@prisma/client";
+    import { Button } from "$lib/components/ui/button/index.js";
 
-    export let experiences: Experience[];
+    import Github from "lucide-svelte/icons/github";
+
+    import type { PersonalProject } from "@prisma/client";
+
+    export let personalProjects: PersonalProject[];
 </script>
 
-<div class="flex flex-col">
+<div class="flex flex-col mt-10">
     <h1
         class="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl mb-4"
     >
-        Experience
+        Personal Projects
     </h1>
 
     <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 my-4 gap-6">
-        {#each experiences as experience}
+        {#each personalProjects as project}
             <Card.Root class="bg-background/50">
                 <Card.Header>
                     <Card.Title class="text-pretty text-primary"
-                        >{experience.project} ({experience.jobTitle})</Card.Title
+                        >{project.name}</Card.Title
                     >
-                    <Card.Description>{experience.company}</Card.Description>
+                    <Card.Description>{project.description}</Card.Description>
                 </Card.Header>
                 <Card.Content class="grid gap-4">
+                    <img src={project.imageURL} alt={project.name} />
                     <h1 class="text-base text-muted-foreground font-semibold">
-                        Responsibilities and activities
+                        Principal characteristics
                     </h1>
-                    <div>
-                        {#each experience.activities as activity}
+                    <div class="mb-4">
+                        {#each project.characteristics as characteristic}
                             <div
                                 class="grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
                             >
@@ -35,20 +40,25 @@
                                 />
                                 <div class="space-y-1">
                                     <p class="text-sm leading-none">
-                                        {activity}
+                                        {characteristic}
                                     </p>
                                 </div>
                             </div>
                         {/each}
                     </div>
-                    <div class="grid grid-cols-3 gap-2 mt-4">
-                        {#each experience.technologies as technology}
+                    <div class="grid grid-cols-3 gap-2 mb-4">
+                        {#each project.technologies as technology}
                             <a class="border text-center" href="/">
                                 {technology}
                             </a>
                         {/each}
                     </div>
                 </Card.Content>
+                <Card.Footer>
+                    <Button class="w-full" href={project.github}>
+                        <Github class="mr-2 h-4 w-4" /> Github
+                    </Button>
+                </Card.Footer>
             </Card.Root>
         {/each}
     </div>
