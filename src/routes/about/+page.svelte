d0302f01dc30785a2784b7b21d4b2113832b034d
Erick Carrasco <erickype@hotmail.com>
2024-05-08 18:56:26 +0000

    Refactor link to resume and social network

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-	import SideBar from "$lib/components/Layout/sideBar.svelte";
+	import SocialNetwork from "$lib/components/util/socialNetwork.svelte";
+
+	import Link from "lucide-svelte/icons/square-arrow-out-up-right";
+
 	import type { PageServerData } from "./$types";
+	import { Button } from "$lib/components/ui/button";
 
 	export let data: PageServerData;
 	const experiences = data.experiences;
@@ -14,18 +18,28 @@
 </div>
 
 <div class="relative w-full p-2">
-	<div class="absolute right-5 top-5">
-		<SideBar />
-	</div>
-	<div class="pb-4">
-		<h3 class="text-xl lg:text-2xl font-bold pb-2">Education</h3>
-		<div class="mb-2">
-			<h4 class="text-md text-primary font-semibold">
-				Bachelor's in Software Engineering
-			</h4>
-			<h5 class="text-sm italic font-thin">
-				Universidad Técnica de Ambato
-			</h5>
+	<div class="flex justify-between items-center">
+		<div class="pb-4">
+			<h3 class="text-xl lg:text-2xl font-bold pb-2">Education</h3>
+			<div class="mb-2">
+				<h4 class="text-md text-primary font-semibold">
+					Bachelor's in Software Engineering
+				</h4>
+				<h5 class="text-sm italic font-thin">
+					Universidad Técnica de Ambato
+				</h5>
+			</div>
+		</div>
+		<div class="grid grid-cols-2 lg:grid-cols-4 items-center justify-center place-content-center">
+			<Button
+				href="resume.pdf"
+				variant="ghost"
+				size="icon"
+				target="_blank"
+			>
+				<Link class="h-4 w-4" />
+			</Button>
+			<SocialNetwork buttonVariant="ghost" />
 		</div>
 	</div>
 	<div class="flex flex-col">
